533f87d44ea413d2484a4e93b0a839cc1f4658bd
Sean Walker <swlkr.rbl@gmail.com>
2017-10-02 19:42:28 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject oksql "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject oksql "1.0.0"
+  :description "A simple clojure postgres library"
+  :url "https://github.com/swlkr/oksql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
